47c856c92b737876457a45c3cade3dd958dc552c
SindhooraDevops <132545161+SindhooraDevops@users.noreply.github.com>
2024-01-22 18:13:27 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -2,5 +2,5 @@ module.exports = Object.freeze({
     DB_HOST : 'database-1-instance-1.cns5nzj6hqny.us-east-1.rds.amazonaws.com',
     DB_USER : 'admin',
     DB_PWD : 'Mysqldatabase',
-    DB_DATABASE : 'database-1'
+    DB_DATABASE : 'database-1-instance-1'
 });
